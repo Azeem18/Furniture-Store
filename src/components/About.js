4a672429395013d2cff9c0c213f17c9a65b1bb83
Azeem18 <azeemahmad0317@gmail.com>
2024-07-26 18:22:20 +0500

    About Section completed

---
+++ src/components/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import AboutGrid from "../components/reuseable-components/AboutGrid";
+import icon1 from "../img/icon1.png";
+import icon2 from "../img/icon2.png";
+import icon3 from "../img/icon3.png";
+import icon4 from "../img/icon4.png";
+import frame1 from "../img/frame1.png";
+import matrix2 from "../img/matrix2.png";
+
+const About = () => {
+  return (
+    <div className=" flex ml-36 mr-20 my-36">
+      <div className=" w-3/5">
+        <h1 className=" text-3xl font-semibold">Why Choose Us</h1>
+        <p className="text-lg mt-7">
+          Donec mattis porta eros, aliquet finibus risus interdum at. Nulla
+          vivethe as it was for us to know what was to be done. the
+        </p>
+        <div className=" grid grid-rows-2 grid-cols-2 gap-y-6 gap-x-6 mt-20">
+          <AboutGrid
+            img={icon1}
+            heading="Fast  & Free Shipping "
+            text="Donec mattis porta eros, aliquet finibus risus interdum at. Nulla vivethe as it was"
+          />
+          <AboutGrid
+            img={icon2}
+            heading="Easy to Shop "
+            text="Donec mattis porta eros, aliquet finibus risus interdum at. Nulla vivethe as it was"
+          />
+          <AboutGrid
+            img={icon3}
+            heading="24/7 Support "
+            text="Donec mattis porta eros, aliquet finibus risus interdum at. Nulla vivethe as it was"
+          />
+          <AboutGrid
+            img={icon4}
+            heading="Hassle Free Returns "
+            text="Donec mattis porta eros, aliquet finibus risus interdum at. Nulla vivethe as it was"
+          />
+        </div>
+      </div>
+      <div className=" z-10 relative ml-32">
+        <img className=" -mt-10 w-5/6 h-5/6" src={frame1} alt="frame"></img>
+        <img
+          className=" absolute -top-14 -left-11 -z-10 h-44 w-64"
+          src={matrix2}
+          alt="Grid"
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/reuseable-components/AboutGrid.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutGrid = (props) => {
+  return (
+    <div className=" relative w-full z-20">
+      <img className=" h-6 w-8" src={props.img} alt="Shipping"></img>
+      <div className=" absolute top-0.5 left-3 -z-10 border rounded-full bg-gray-400 w-8 h-8 "></div>
+      <h1 className=" text-xl font-semibold mt-5">{props.heading}</h1>
+      <p className=" mt-3 ">{props.text}</p>
+    </div>
+  );
+};
+
+export default AboutGrid;
