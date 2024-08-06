a964335092b392b7406be0df6b49e116bc535390
Azeem18 <azeemahmad0317@gmail.com>
2024-08-06 08:52:27 +0500

    Hover effect on footer links removed

--- src/components/reuseable-components/Footerlinks.js
+++ src/components/reuseable-components/Footerlinks.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footertags = (props) => {
   return (
-    <p className=" text-slate-800 text-lg mb-2 cursor-pointer hover:text-slate-500 hover:text-xl">
+    <p className=" text-slate-800 text-lg mb-2 cursor-pointer hover:text-slate-500">
       {props.text}
     </p>
   );
